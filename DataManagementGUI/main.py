02828af51e1c3800281996610824376152e516c6
Connor Fulton <cfulton117@gmail.com>
2018-07-10 17:38:33 +0100

    reorganized ui, slider and pause functions working with webcam and video files, cleaned up code

--- DataManagementGUI/main.py
+++ DataManagementGUI/main.py
@@ -7,61 +7,45 @@ import cv2
 import threading
 import time
 import qtmodern
+from retinavision.retina import Retina
+from retinavision.cortex import Cortex
+from retinavision import datadir, utils
 from QtCore import QThread, pyqtSignal, Qt, QTimer
 from QtGui import QImage, QPixmap
 from QtWidgets import QApplication, QMainWindow, QInputDialog, QFileDialog, QWidget
 from qtmodern import styles, windows
 
-#class Thread(QThread):
-#
-#    changePixmap = pyqtSignal(QImage)
-#    videoName = None
-#    def __init__(self,fileName,parent=None):
-#        super(Thread,self).__init__()
-#        self.videoName = fileName
-#
-#    def run(self):
-#        if self.videoName:
-#            cap = cv2.VideoCapture(self.videoName)
-#            codec = cv2.VideoWriter_fourcc(*'mp4v')
-#            cap.set(cv2.CAP_PROP_FOURCC, codec)
-#        else:
-#            cap = cv2.VideoCapture(0)
-#        cv2.waitKey(0)
-#        while not QThread.currentThread().isInterruptionRequested():
-#            ret, frame = cap.read()
-#            rgbImage = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-#            converttoQtFormat = QImage(rgbImage.data,rgbImage.shape[1],rgbImage.shape[0],QImage.Format_RGB888)
-#            pic = converttoQtFormat.scaled(640,480,Qt.KeepAspectRatio)
-#            self.changePixmap.emit(pic)
-#        cap.release()
-
 class VideoPlayer(QWidget):
 
-    changePixmap = pyqtSignal(QImage)
     videoName = None
     videoFrame = None
+    framePos = 0
+    maxFrames = 0
 
     def __init__(self,fileName,parent):
         super(QWidget,self).__init__()
         self.videoName = fileName
+        self.parent = parent
         if self.videoName:
             self.cap = cv2.VideoCapture(self.videoName)
             codec = cv2.VideoWriter_fourcc(*'mp4v')
             self.cap.set(cv2.CAP_PROP_FOURCC, codec)
+            self.maxFrames = self.cap.get(cv2.CAP_PROP_FRAME_COUNT)
+            self.parent.scrubSlider.maximum = self.maxFrames
         else:
             self.cap = cv2.VideoCapture(0)
         self.videoFrame = parent.label
 
     def nextFrame(self):
         ret, frame = self.cap.read()
-        rgbImage = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-        converttoQtFormat = QImage(rgbImage.data,rgbImage.shape[1],rgbImage.shape[0],QImage.Format_RGB888)
-        pic = converttoQtFormat.scaled(640,480,Qt.KeepAspectRatio)
-        #self.changePixmap.emit(pic)
-        self.videoFrame.setPixmap(QPixmap.fromImage(pic))
-
-        #setPixmap done in setImage of DMApp class - move it here?
+        if ret:
+            framePos = self.cap.get(cv2.CAP_PROP_POS_FRAMES)
+            rgbImage = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+            converttoQtFormat = QImage(rgbImage.data,rgbImage.shape[1],rgbImage.shape[0],QImage.Format_RGB888)
+            pic = converttoQtFormat.scaled(480,360,Qt.KeepAspectRatio)
+            self.videoFrame.setPixmap(QPixmap.fromImage(pic))
+            self.parent.scrubSlider.setValue(framePos)
+            self.parent.frameNum.display(framePos)
 
     def start(self):
         self.timer = QTimer()
@@ -73,6 +57,11 @@ class VideoPlayer(QWidget):
             self.timer.stop()
         else:
             self.timer.start()
+
+    def skip(self, framePos):
+        self.cap.set(cv2.CAP_PROP_POS_FRAMES, framePos)
+        self.parent.scrubSlider.setValue(framePos)
+        self.parent.frameNum.display(framePos)
 
 class DMApp(QMainWindow, design.Ui_MainWindow):
 
@@ -89,13 +78,11 @@ class DMApp(QMainWindow, design.Ui_MainWindow):
         self.setupUi(self)
         self.webcamButton.clicked[bool].connect(self.runWebCam)
         self.browseButton.clicked.connect(self.openFileNameDialog)
-        #self.pauseButton.clicked[bool].connect(self.videoPlayer.pause)
-        #self.startVideoPlayer()
 
     def openFileNameDialog(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileName(self,"Open file", "","All Files (*);;Python Files (*.py)", options=options)
+        fileName, _ = QFileDialog.getOpenFileName(self,"Open file", "","All Files (*);;mp4 Files (*.mp4)", options=options)
         if fileName:
             print("Opening " + fileName)
             self.fileName = fileName
@@ -109,39 +96,27 @@ class DMApp(QMainWindow, design.Ui_MainWindow):
         if fileName:
             print(fileName)
 
-#    def runThread(self, fileName):
-#        self.vidThread = Thread(fileName)
-#        self.vidThread.changePixmap.connect(self.setImage)
-#        self.timer = QTimer()
-#        self.timer.moveToThread(self.vidThread)
-#        self.vidThread.start()
-
     def startVideoPlayer(self):
         if not self.videoPlayer:# and self.isVideoLoaded:
             self.videoPlayer = VideoPlayer(self.fileName, self)
             self.pauseButton.clicked[bool].connect(self.videoPlayer.pause)
+            self.scrubSlider.valueChanged.connect(self.sendFramePos)
         self.videoPlayer.start()
+
+    def sendFramePos(self):
+        framePos = self.scrubSlider.value()
+        self.videoPlayer.skip(framePos)
 
     def runWebCam(self, event):
         if event:
-            #self.runThread(None)
             self.fileName = 0
             self.isVideoLoaded = True
             self.startVideoPlayer()
             self.browseButton.setDisabled(True)
+            self.scrubSlider.setDisabled(True)
         else:
             self.videoPlayer = None
-            #self.videoPlayer.pause(event)
             self.browseButton.setDisabled(False)
-
-#    def setImage(self, image):
-#        self.label.setPixmap(QPixmap.fromImage(image))
-
-#    def pause(self, event):
-#        if event:
-#            self.timer.stop()
-#        else:
-#            self.timer.start()
 
 def main():
     app = QApplication(sys.argv)
