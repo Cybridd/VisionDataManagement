66d4432e81c15919e0f30fa06707b8cc4adca227
Connor Fulton <cfulton117@gmail.com>
2018-08-28 15:02:02 +0100

    packaging for installation

---
+++ README.md
@@ -0,0 +1,13 @@
+# VisionDataManagement
+
+
+## Requirements
+pandas
+opencv_python
+h5py
+numpy
+PyQt5
+retinavision
+
+
+## Installation
---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+setup(
+    name="visiondatamanagement",
+    version="0.1",
+    author="Connor Fulton",
+    author_email="cfulton117@gmail.com",
+    description="A tool for the management of data produced by the RetinaVision system.",
+    packages=["visiondatamanagement"],
+    classifiers=(
+        "Programming Language :: Python :: 2",
+        "Operating System :: OS Independent",
+    ),
+    long_description=read('README.md'),
+    install_requires=[read(os.path.join('visiondatamanagement','requirements.txt'))],
+    dependency_links=[
+    "https://Cybridd:Jaguar82!@github.com/Pozimek/RetinaVision@1693fbcaad0813a0bc8937b6dd4288cbdd273e4c#egg=retinavision-0.9",
+    "git://github.com/pyqt/python-qt5#egg=python-qt5",
+    ],
+)
+#@1693fbcaad0813a0bc8937b6dd4288cbdd273e4c
